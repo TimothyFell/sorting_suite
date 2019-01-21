3ac64f9e9b0f0c48af7065845bd5f5dd9b4973e1
TimothyFell <timothy.fell846@gmail.com>
2019-01-21 12:36:09 -0700

    Add ruby reference

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,9 +1,24 @@
+// def sort
+//   return cards if cards.length <= 1
+//   flip = true
+//   while flip do
+//     flip = false
+//     0.upto(cards.length - 2) do |i|
+//       if cards[i].card_id > cards[i + 1].card_id
+//         cards[i], cards[i + 1] = cards[i + 1], cards[i]
+//         flip = true
+//       end
+//     end
+//   end
+//   cards
+// end
+
 function bubbleSort(a) {
   sa = Array.from(a);
-  let flip;
+  var flip;
   do {
     flip = false;
-    for (let i = 1; i < sa.length; ++i) {
+    for (var i = 1; i < sa.length; ++i) {
       if (sa[i - 1] > sa[i]) {
         [ sa[i], sa[i - 1] ] = [ sa[i - 1], sa[i] ];
         flip = true;
