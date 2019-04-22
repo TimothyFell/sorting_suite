a0e4f3ebd65297f0f033038cedf39a6d3880f82c
TimothyFell <timothy.fell846@gmail.com>
2019-04-22 13:03:02 -0600

    add second insertion solution that is more memory efficient

--- insertionSort.js
+++ insertionSort.js
@@ -1,29 +1,48 @@
+// first solution - creates a new array to add and sort each element from original array. time is O(n^2) and memory is 2n.
+// function insertionSort(a) {
+//   // return original array if length = 0 or 1
+//   if (a.length === 1 || a.length === 0){
+//     return a;
+//   }
+//   var sa = [];
+//   var current_index = 0;
+//   while (current_index < a.length) {
+//     if (sa.length === 0) {
+//       sa.push(a[current_index])
+//     } else if (a[current_index] > sa[sa.length - 1]) {
+//       sa.push(a[current_index]);
+//     } else {
+//       sa_length = sa.length
+//       for (var i = 0; i < sa_length;) {
+//         if (a[current_index] < sa[i] || a[current_index] === sa[i]) {
+//            sa.splice(i, 0, a[current_index])
+//            i = sa_length
+//         } else {
+//           i++
+//         }
+//       }
+//     }
+//     current_index++
+//   }
+//   return sa
+// }
+
+// second solution - doesnt create a new array. time is O(n^2) and memory is 1n.
 function insertionSort(a) {
-  // return original array if length = 0 or 1
   if (a.length === 1 || a.length === 0){
-    return a;
+      return a;
   }
-  var sa = [];
-  var current_index = 0;
-  while (current_index < a.length) {
-    if (sa.length === 0) {
-      sa.push(a[current_index])
-    } else if (a[current_index] > sa[sa.length - 1]) {
-      sa.push(a[current_index]);
-    } else {
-      sa_length = sa.length
-      for (var i = 0; i < sa_length;) {
-        if (a[current_index] < sa[i] || a[current_index] === sa[i]) {
-           sa.splice(i, 0, a[current_index])
-           i = sa_length
-        } else {
-          i++
-        }
-      }
+  for (var i = 0; i < a.length; i++) {
+    var current_e = a[i]
+    var index = i
+    while (index > 0 && current_e < a[index - 1]) {
+      a[index] = a[index - 1]
+      index = index - 1
     }
-    current_index++
+    a[index] = current_e
   }
-  return sa
+  return a
 }
+
 
 module.exports = insertionSort
