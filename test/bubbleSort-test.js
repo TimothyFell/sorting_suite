26d9788d18e33ff2d438beee284f59964e53d7b9
TimothyFell <timothy.fell846@gmail.com>
2019-04-22 11:11:31 -0600

    Updated bubble sort tests to match new merge tests

--- test/bubbleSort-test.js
+++ test/bubbleSort-test.js
@@ -14,11 +14,47 @@ describe('Bubble Sort', function() {
     expect(sorted[3]).to.equal(expected[3]);
     expect(sorted[4]).to.equal(expected[4]);
   })
+  it('it can sort an array of numbers with duplicates', function(){
+    var initial = [6,4,2,6,2];
+    var sorted = bubbleSort(initial);
+
+    var expected = [2,2,4,6,6];
+
+    expect(sorted[0]).to.equal(expected[0]);
+    expect(sorted[1]).to.equal(expected[1]);
+    expect(sorted[2]).to.equal(expected[2]);
+    expect(sorted[3]).to.equal(expected[3]);
+    expect(sorted[4]).to.equal(expected[4]);
+  })
   it('it can sort an array of letters', function(){
     var initial = ['c','a','z','g','t'];
     var sorted = bubbleSort(initial);
 
     var expected = ['a','c','g','t','z'];
+
+    expect(sorted[0]).to.equal(expected[0]);
+    expect(sorted[1]).to.equal(expected[1]);
+    expect(sorted[2]).to.equal(expected[2]);
+    expect(sorted[3]).to.equal(expected[3]);
+    expect(sorted[4]).to.equal(expected[4]);
+  })
+  it('it can sort an array of letters with duplicates', function(){
+    var initial = ['g','a','z','g','a'];
+    var sorted = bubbleSort(initial);
+
+    var expected = ['a','a','g','g','z'];
+
+    expect(sorted[0]).to.equal(expected[0]);
+    expect(sorted[1]).to.equal(expected[1]);
+    expect(sorted[2]).to.equal(expected[2]);
+    expect(sorted[3]).to.equal(expected[3]);
+    expect(sorted[4]).to.equal(expected[4]);
+  })
+  it('it can sort an array of words, not just letters', function(){
+    var initial = ['groza','awm','zebra','gbetto','awesome'];
+    var sorted = bubbleSort(initial);
+
+    var expected = ['awesome','awm','gbetto','groza','zebra'];
 
     expect(sorted[0]).to.equal(expected[0]);
     expect(sorted[1]).to.equal(expected[1]);
